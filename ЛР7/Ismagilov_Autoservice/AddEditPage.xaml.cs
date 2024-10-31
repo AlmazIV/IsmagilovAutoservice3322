35cc4c87ee52fe35026160fd3c2b78c5cca400d9
AlmazIV <almaz.ismagilov2018@gmail.com>
2024-10-31 12:25:53 +0300

    Лабораторная работа 7

---
+++ ЛР7/Ismagilov_Autoservice/AddEditPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ismagilov_Autoservice
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        //добавим новое поле, которое будет хранить в себе экземпляр добавляемого сервиса
+        private Service _currentService = new Service();
+        public AddEditPage(Service SelectedService)
+        {
+            InitializeComponent();
+
+            if (SelectedService != null)
+                _currentService = SelectedService;
+            //при инициализации установим DataContext страницы - этот созданный объект
+            DataContext = _currentService;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentService.Title))
+                errors.AppendLine("Укажите название услуги");
+
+            if (_currentService.Cost <= 0)
+                errors.AppendLine("укажите стоимость услуги");
+
+            if (_currentService.DiscountInt > 100 || _currentService.DiscountInt < 0 || _currentService.Discount.HasValue == false)
+                errors.AppendLine("Укажите скидку");
+
+            if (string.IsNullOrWhiteSpace(_currentService.DurationInSeconds))
+                errors.AppendLine("Укажите длительность услуги");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            if (_currentService.ID == 0)
+                IsmagilovAutoserviceEntities.GetContext().Service.Add(_currentService);
+
+            try
+            {
+                IsmagilovAutoserviceEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
